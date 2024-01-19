bad4c5f83263e44b0220de08d9fdbc3e48273564
Owusu Kelvin Clark <kelvinclark2k77@gmail.com>
2024-01-19 15:04:50 +0100

    Lower logging level

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,16 @@ use umya_spreadsheet::reader;
 
 #[tokio::main]
 async fn main() {
-    // tracing_subscriber::fmt::init();
-    // let app = axum_starter::get_app_router().await.unwrap();
+    tracing_subscriber::fmt::fmt()
+        .with_env_filter("axum_starter=trace")
+        .event_format(tracing_subscriber::fmt::format().pretty())
+        .init();
+    let app = axum_starter::get_app_router().await.unwrap();
 
-    // let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
-    //     .await
-    //     .unwrap();
-    // axum::serve(listener, app).await.unwrap();
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
+        .await
+        .unwrap();
+    axum::serve(listener, app).await.unwrap();
     // let file = reader::xlsx::read(r#".\data\0329 giao hang.xlsx"#).unwrap();
     // let sheet = file.get_sheet(&0usize).unwrap();
     // let a = sheet.get_cell_collection()[0];
@@ -23,7 +26,7 @@ async fn main() {
     // println!("{}", i);
     // }
     // println!("{}", (1..=3).contains(&3));
-    let mut a = String::from("hello");
-    a.replace_range(1..=1, "k");
-    println!("{}", a);
+    // let mut a = String::from("hello");
+    // a.replace_range(1..=1, "k");
+    // println!("{}", a);
 }
